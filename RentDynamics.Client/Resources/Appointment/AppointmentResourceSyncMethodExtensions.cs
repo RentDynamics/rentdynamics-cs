57fa279a80577d2d9e65df541f452b42d95f143d
dmitry-vychikov <dmitry.vychikov@dsr-company.com>
2020-08-13 14:05:08 +0300

    Add sync methods for appointment resource

---
+++ RentDynamics.Client/Resources/Appointment/AppointmentResourceSyncMethodExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using RentDynamics.Client.Models;
+
+namespace RentDynamics.Client.Resources
+{
+    public static class AppointmentResourceSyncMethodExtensions
+    {
+        public static List<DateTime> GetAppointmentTimes(this AppointmentResource resource, int communityGroupId, DateTime appointmentDate, bool isUtc)
+            => resource.GetAppointmentTimesAsync(communityGroupId, appointmentDate, isUtc).GetAwaiter().GetResult();
+
+        public static AppointmentDays GetAppointmentDays(this AppointmentResource resource, int communityGroupId, DateTime startAppointmentDate, DateTime endAppointmentDate)
+            => resource.GetAppointmentDaysAsync(communityGroupId, startAppointmentDate, endAppointmentDate).GetAwaiter().GetResult();
+    }
+}
