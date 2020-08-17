754e80614e5c2429717686a118d12540eee7a756
dmitry-vychikov <dmitry.vychikov@dsr-company.com>
2020-08-17 13:23:43 +0300

    Add AuthenticationResource unit tests

---
+++ RentDynamics.RdClient.Tests/ResourcesDataSerialization/AuthenticationResourceTests.cs
@@ -0,0 +1,71 @@
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq.Contrib.HttpClient;
+using Newtonsoft.Json.Linq;
+using RentDynamics.RdClient.Resources;
+
+namespace RentDynamics.RdClient.Tests.ResourcesDataSerialization
+{
+    [TestClass]
+    public class AuthenticationResourceTests : BaseResourceDataSerializationTest<AuthenticationResource>
+    {
+        [TestMethod]
+        public async Task LoginMoqEndpoint()
+        {
+            const string username = "user";
+            const string password = "password";
+            using var sha = new SHA1Managed();
+            string hashedPassword = sha.ComputeHash(Encoding.UTF8.GetBytes(password)).ToHexString().ToLower();
+
+            MockHandler.SetupAnyRequest()
+                       .ReturnsResponse(HttpStatusCode.OK, "{\"userId\":100500, \"token\":\"myResponseToken\"}", MediaTypeNames.Application.Json);
+
+
+            var loginResponse = await Resource.LoginAsync(username, password);
+
+            MockHandler.VerifyRequest(async message =>
+            {
+                message.RequestUri.PathAndQuery.Should().Be("/auth/login");
+
+                var jContent = await message.Content.ReadAsAsync<JObject>();
+                jContent.Property("username").Value.ToString().Should().Be(username);
+                jContent.Property("password").Value.ToString().Should().Be(hashedPassword);
+                return true;
+            });
+
+            loginResponse.AuthenticationToken.Should().Be("myResponseToken");
+            loginResponse.UserId.Should().Be(100500);
+
+            Options.UserAuthentication.IsAuthenticated.Should().BeTrue();
+            Options.UserAuthentication.UserId.Should().Be(100500);
+            Options.UserAuthentication.AuthenticationToken.Should().Be("myResponseToken");
+        }
+
+        [TestMethod]
+        public async Task LogoutMoqEndpoint()
+        {
+            Options.UserAuthentication.SetAuthentication(100500, "myToken");
+
+            MockHandler.SetupAnyRequest()
+                       .ReturnsResponse(HttpStatusCode.NoContent, Stream.Null);
+
+            await Resource.LogoutAsync();
+
+            MockHandler.VerifyRequest(message =>
+            {
+                message.RequestUri.PathAndQuery.Should().Be("/auth/logout");
+
+                return true;
+            });
+
+            Options.UserAuthentication.IsAuthenticated.Should().BeFalse();
+        }
+    }
+}
--- RentDynamics.RdClient.Tests/ResourcesDataSerialization/BaseResourceDataSerializationTest.cs
+++ RentDynamics.RdClient.Tests/ResourcesDataSerialization/BaseResourceDataSerializationTest.cs
@@ -8,25 +8,27 @@ using RentDynamics.RdClient.Resources;
 namespace RentDynamics.RdClient.Tests.ResourcesDataSerialization
 {
     public abstract class BaseResourceDataSerializationTest<TResource>
-    where TResource: BaseRentDynamicsResource
+        where TResource : BaseRentDynamicsResource
     {
         protected Mock<HttpMessageHandler> MockHandler { get; private set; } = null!;
-        protected virtual TResource Resource => (TResource) Activator.CreateInstance(typeof(TResource), CreateRdClient())!; 
+        protected RentDynamicsOptions Options { get; private set; }
+
+        protected virtual TResource Resource => (TResource) Activator.CreateInstance(typeof(TResource), CreateRdClient())!;
 
         [TestInitialize]
         public virtual void TestInitialize()
         {
-            MockHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-        }
-        
-        protected virtual RentDynamicsApiClient CreateRdClient()
-        {
-            return CreateRdClient(MockHandler);
+            MockHandler = new Mock<HttpMessageHandler>();
+            Options = new RentDynamicsOptions("test", "test-secret", isDevelopment: true);
         }
 
-        protected static RentDynamicsApiClient CreateRdClient(Mock<HttpMessageHandler> mockHandler)
+        protected virtual RentDynamicsApiClient CreateRdClient()
         {
-            var options = new RentDynamicsOptions("test", "test-secret", isDevelopment: true);
+            return CreateRdClient(MockHandler, Options);
+        }
+
+        protected static RentDynamicsApiClient CreateRdClient(Mock<HttpMessageHandler> mockHandler, RentDynamicsOptions options)
+        {
             var httpClient = mockHandler.CreateClientWithBaseAddress();
             return new RentDynamicsApiClient(httpClient, new RentDynamicsApiClientSettings { Options = options });
         }
