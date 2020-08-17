0a8731ee7fb181b6f549f6e246f38b309ecf710e
dmitry-vychikov <dmitry.vychikov@dsr-company.com>
2020-08-17 14:13:26 +0300

    Add DeserializationTests

---
+++ RentDynamics.RdClient.Tests/ResourcesDataSerialization/DeserializationTests.cs
@@ -0,0 +1,61 @@
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq.Contrib.HttpClient;
+using Newtonsoft.Json.Linq;
+using RentDynamics.RdClient.Resources;
+
+namespace RentDynamics.RdClient.Tests.ResourcesDataSerialization
+{
+    [TestClass]
+    public class DeserializationTests : BaseResourceDataSerializationTest<BaseRentDynamicsResource>
+    {
+        public class MyModel
+        {
+            public int ID { get; set; }
+            public string? NameProperty { get; set; }
+        }
+
+        [TestMethod]
+        public async Task Model_WithPascalCaseProperties_ShouldBeDeserialized_WhenResponseContainsCamelCaseProperties()
+        {
+            MockHandler.SetupAnyRequest()
+                       .ReturnsResponse(HttpStatusCode.OK, "{\"id\":100500, \"nameProperty\":\"MyTestName\"}", MediaTypeNames.Application.Json);
+
+
+            var myModel = await CreateRdClient().GetAsync<MyModel>("");
+
+            myModel.ID.Should().Be(100500);
+            myModel.NameProperty.Should().Be("MyTestName");
+        }
+
+        [TestMethod]
+        public async Task Model_ShouldWithPascalCaseProperties_ShouldBeSerializer_IntoCamelCaseJson()
+        {
+            MockHandler.SetupAnyRequest()
+                       .ReturnsResponse(HttpStatusCode.NoContent, Stream.Null);
+
+            var myModel = new MyModel
+            {
+                ID = 100500,
+                NameProperty = "MyTestName"
+            };
+
+            await CreateRdClient().PostAsync<MyModel, object>("", myModel);
+
+            MockHandler.VerifyRequest(async message =>
+            {
+                var jContent = await message.Content.ReadAsAsync<JObject>();
+
+                jContent.Should().ContainKey("id").WhichValue.Value<int>().Should().Be(100500);
+                jContent.Should().ContainKey("nameProperty").WhichValue.Value<string>().Should().Be("MyTestName");
+
+                return true;
+            });
+        }
+    }
+}
