ad2cb5e0839549077851f4c81234a933dfc2524b
dmitry-vychikov <dmitry.vychikov@dsr-company.com>
2020-08-19 11:46:26 +0300

    Sort objects inside json arrays for nonce calculation

--- RentDynamics.RdClient/JsonSortHelper.cs
+++ RentDynamics.RdClient/JsonSortHelper.cs
@@ -5,19 +5,48 @@ namespace RentDynamics.RdClient
 {
     public static class JsonSortHelper
     {
-        public static void Sort(JObject jObject)
+        private static void SortJArray(JArray jArray)
+        {
+            foreach (JToken jToken in jArray)
+            {
+                Sort(jToken);
+            }
+        }
+
+        private static void SortJObject(JObject jObject)
         {
             var properties = jObject.Properties().ToArray();
 
-            jObject.RemoveAll();
+            foreach (JProperty jProperty in properties)
+            {
+                jProperty.Remove();
+            }
 
             foreach (JProperty jProperty in properties.OrderBy(a => a.Name))
             {
                 jObject.Add(jProperty);
-                if (jProperty.Value is JObject propertyObject)
-                {
-                    Sort(propertyObject);
-                }
+                SortJProperty(jProperty);
+            }
+        }
+
+        private static void SortJProperty(JProperty jProperty)
+        {
+            Sort(jProperty.Value);
+        }
+
+        public static void Sort(JToken jToken)
+        {
+            switch (jToken)
+            {
+                case JArray jArray:
+                    SortJArray(jArray);
+                    break;
+                case JObject jObject:
+                    SortJObject(jObject);
+                    break;
+                case JProperty jProperty:
+                    SortJProperty(jProperty);
+                    break;
             }
         }
     }
