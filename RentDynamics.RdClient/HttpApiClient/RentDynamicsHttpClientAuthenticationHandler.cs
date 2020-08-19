6944f3448d158c952fd2453a5506427fda8bdb64
dmitry-vychikov <dmitry.vychikov@dsr-company.com>
2020-08-19 10:25:06 +0300

    Reformat code in RentDynamicsHttpClientAuthenticationHandler.cs

--- RentDynamics.RdClient/HttpApiClient/RentDynamicsHttpClientAuthenticationHandler.cs
+++ RentDynamics.RdClient/HttpApiClient/RentDynamicsHttpClientAuthenticationHandler.cs
@@ -9,39 +9,39 @@ namespace RentDynamics.RdClient.HttpApiClient
         where TClientSettings : IRentDynamicsApiClientSettings
 
     {
-    private readonly TClientSettings _settings;
-    private readonly INonceCalculator _nonceCalculator;
-    
-    private RentDynamicsOptions Options => _settings.Options;
+        private readonly TClientSettings _settings;
+        private readonly INonceCalculator _nonceCalculator;
 
-    public RentDynamicsHttpClientAuthenticationHandler(TClientSettings settings, INonceCalculator? nonceCalculator = null)
-    {
-        _settings = settings;
-        _nonceCalculator = nonceCalculator ?? new NonceCalculator();
-    }
+        private RentDynamicsOptions Options => _settings.Options;
 
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        var unixEpoch = new DateTime(1970, 1, 1);
-        long unixTimestampMilliseconds = (long) (DateTime.UtcNow - unixEpoch).TotalMilliseconds;
-
-        string? requestContent = request.Content == null
-            ? null
-            : await request.Content.ReadAsStringAsync();
-
-        string nonce = _nonceCalculator.GetNonce(Options.ApiSecretKey, unixTimestampMilliseconds, request.RequestUri.PathAndQuery, requestContent);
-
-        request.Headers.Add("x-rd-api-key", Options.ApiKey);
-        request.Headers.Add("x-rd-timestamp", unixTimestampMilliseconds.ToString());
-        request.Headers.Add("x-rd-api-nonce", nonce);
-
-        var userAuthentication = Options.UserAuthentication;
-        if (userAuthentication.IsAuthenticated)
+        public RentDynamicsHttpClientAuthenticationHandler(TClientSettings settings, INonceCalculator? nonceCalculator = null)
         {
-            request.Headers.Add("Authorization", $"TOKEN {userAuthentication.AuthenticationToken}");
+            _settings = settings;
+            _nonceCalculator = nonceCalculator ?? new NonceCalculator();
         }
 
-        return await base.SendAsync(request, cancellationToken);
-    }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var unixEpoch = new DateTime(1970, 1, 1);
+            long unixTimestampMilliseconds = (long) (DateTime.UtcNow - unixEpoch).TotalMilliseconds;
+
+            string? requestContent = request.Content == null
+                ? null
+                : await request.Content.ReadAsStringAsync();
+
+            string nonce = _nonceCalculator.GetNonce(Options.ApiSecretKey, unixTimestampMilliseconds, request.RequestUri.PathAndQuery, requestContent);
+
+            request.Headers.Add("x-rd-api-key", Options.ApiKey);
+            request.Headers.Add("x-rd-timestamp", unixTimestampMilliseconds.ToString());
+            request.Headers.Add("x-rd-api-nonce", nonce);
+
+            var userAuthentication = Options.UserAuthentication;
+            if (userAuthentication.IsAuthenticated)
+            {
+                request.Headers.Add("Authorization", $"TOKEN {userAuthentication.AuthenticationToken}");
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
     }
 }
