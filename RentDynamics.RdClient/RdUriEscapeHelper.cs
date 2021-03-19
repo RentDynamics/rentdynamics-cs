0e037833dce5b57e6ac80a02a4917ce7cc676011
dmitry-vychikov <dmitry.vychikov@dsr-company.com>
2021-03-19 23:27:36 +0300

    Unescape whitespace characters prior to nonce calculation

--- RentDynamics.RdClient/RdUriEscapeHelper.cs
+++ RentDynamics.RdClient/RdUriEscapeHelper.cs
@@ -4,8 +4,10 @@ namespace RentDynamics.RdClient
     {
         public static string UnescapeSpecialRdApiCharacters(string url)
         {
-            //Pipe '|' character is treated specially in RD api. Details: https://github.com/Skylude/django-rest-framework-signature/blob/master/rest_framework_signature/authentication.py#L132
-            return url.Replace("%7C", "|");
+            //Nonce calculator in python RD-api decodes only %7C (pipe - '|') and %20 (whitespace - ' '), everything else remains URL encoded. 
+            //Details: https://github.com/Skylude/django-rest-framework-signature/blob/master/rest_framework_signature/authentication.py#L132
+            return url.Replace("%7C", "|")
+                      .Replace("%20", " ");
         }
     }
 }
